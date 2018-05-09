0fd3d27510ceb1009ae2247710480d004b878525
kklin4 <kklin4@illinois.edu>
2018-05-08 21:30:38 -0500

    Implemented primitive versions of Movement, Walls, Spike, HealthBar

---
+++ Kevin Alice Game/Assets/Environment/Spike.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	/**
+     * Damages enemies upon impact. 
+     */
+	void OnCollisionEnter2D (Collision2D collision) {
+		Entity collisionGameObjectEntity = collision.gameObject.GetComponent<Entity>();
+		if (collisionGameObjectEntity != null) {
+			collisionGameObjectEntity.TakeDiscreteDamage(10f);
+		}
+	}
+}
---
+++ Kevin Alice Game/Assets/HUD/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+	public Entity targetEntity;
+	protected Transform healthBarContainerTransform;
+	protected Vector3 healthBarContainerTransformPositionOffset;
+
+	// Use this for initialization
+	protected void Start () {
+		healthBarContainerTransform = transform.parent;
+		healthBarContainerTransformPositionOffset = new Vector3(0f, 0.6f);
+	}
+
+	// Update is called once per frame
+	protected void Update () {
+		//healthBarContainerTransform.position = Camera.main.WorldToScreenPoint (new Vector2 (targetTransform.position.x, targetTransform.position.y + 0.6f));
+		//Vector3 forwardPosition = Camera.main.transform.TransformPoint(new Vector3(0, 0.6f));
+		//resourceBarContainerTransform.position = Camera.main.WorldToScreenPoint(targetTransform.position + forwardPosition - Camera.main.transform.position);
+		healthBarContainerTransform.position = targetEntity.transform.position + healthBarContainerTransformPositionOffset;
+		transform.localScale = new Vector3(targetEntity.health / targetEntity.maxHealth, 1f, 1f);
+	}
+}
